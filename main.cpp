748d5723767a03e974afcf0e43dd6561d792cd3d
martinquattro <mquattrone@fi.uba.ar>
2023-09-14 00:24:29 +0000

    Comments added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
+/*!****************************************************************************
+ * @file main.cpp
+ * @brief Activate the alarm when gas or overTemperature is detected
+ * This program was built and tested on the NUCLEO F429ZI ARMC6,
+ * connected to a Dip Switch and it takes care of turning on and off
+ * the LED1 based on the selected state of the dip switch. Additionally,
+ * it prints in the terminal the states of the pins
+ * The pins is set into a PullDown mode so the dip switch should be connected 
+ * to 3.3V pin
+ * There is an OFF combination of buttons to set the alarm state to OFF and so its pin LED
+ * @author Quattrone Martin
+ * @author Vazquez Rodrigo
+ *******************************************************************************/
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 
@@ -23,16 +37,27 @@ int main()
 
     bool alarmState = OFF;
 
-    while (true) {
+    while (true) 
+    {
 
-        if ( gasDetector || overTempDetector ) {
+        if ( gasDetector || overTempDetector ) 
+        {
             alarmState = ON;
+            printf("if ( gasDetector || overTempDetector ) \n");                    //!< The state of the pins are printed
+            printf("%s = %d\n","gasDetector = ", gasDetector.read());
+            printf("%s = %d\n","overTempDetector = ", overTempDetector.read());
+            printf("%s = %d\n","alarmState = ", alarmState);
         }
 
         alarmLed = alarmState;
 
-        if ( aButton && bButton && !cButton && !dButton) {
+        if ( aButton && bButton && !cButton && !dButton) 
+        {
             alarmState = OFF;
+            printf("if ( alarmOffButton ) \n");                                     //!< The state of the pins are  printed
+            printf("%s = %d\n","gasDetector = ", gasDetector.read());
+            printf("%s = %d\n","overTempDetector = ", overTempDetector.read());
+            printf("%s = %d\n","alarmState = ", alarmState);
         }
     }
 }
